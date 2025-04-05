2506949005475ccccf5302e13e898ae201dc30f1
Gabriele Consiglio <4226595+ilmeskio@users.noreply.github.com>
2025-04-05 11:03:33 +0200

    divided database

---
+++ __tests__/integration/api.v2.spec.ts
@@ -0,0 +1,111 @@
+import {addTodo, deleteTodo, editTodo, getAllTodos} from '@/api.v2';
+import {expect, test} from '@jest/globals';
+
+test('GET all TODOs', async () => {
+    const actual = await getAllTodos();
+    expect(actual).toEqual([
+        {
+            "id": "1",
+            "text": "Test 1",
+            "done": false
+        },
+        {
+            "id": "2",
+            "text": "Test 2",
+            "done": false
+        },
+        {
+            "id": "3",
+            "text": "Test 3",
+            "done": false
+        }
+    ]);
+});
+
+test('add a TODO', async () => {
+    await addTodo({
+        id: "4",
+        text: "Test 4",
+        done: false
+    });
+    const actual = await getAllTodos();
+
+    expect(actual).toEqual([
+        {
+            id: "1",
+            text: "Test 1",
+            done: false
+        },
+        {
+            id: "2",
+            text: "Test 2",
+            done: false
+        },
+        {
+            id: "3",
+            text: "Test 3",
+            done: false
+        },
+        {
+            id: "4",
+            text: "Test 4",
+            done: false
+        }
+    ]);
+});
+
+test('edit a TODO', async () => {
+    await editTodo({
+        id: "2",
+        text: "Test 2 edited",
+        done: false
+    });
+    const actual = await getAllTodos();
+
+    expect(actual).toEqual([
+        {
+            id: "1",
+            text: "Test 1",
+            done: false
+        },
+        {
+            id: "2",
+            text: "Test 2 edited",
+            done: false
+        },
+        {
+            id: "3",
+            text: "Test 3",
+            done: false
+        },
+        {
+            id: "4",
+            text: "Test 4",
+            done: false
+        }
+    ]);
+});
+
+test('delete a TODO', async () => {
+
+    await deleteTodo('2');
+    const actual = await getAllTodos();
+
+    expect(actual).toEqual([
+        {
+            id: "1",
+            text: "Test 1",
+            done: false
+        },
+        {
+            id: "3",
+            text: "Test 3",
+            done: false
+        },
+        {
+            id: "4",
+            text: "Test 4",
+            done: false
+        }
+    ]);
+});
--- types/tasks.ts
+++ types/tasks.ts
@@ -2,3 +2,10 @@ export interface ITask {
   id: string,
   text: string
 }
+
+
+export interface ITaskV2 {
+  id: string,
+  text: string,
+  done: boolean
+}
